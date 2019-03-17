a36da5e322fb581b516cf9febaca6ae97f7958db
Chandrakant <cadaats@gmail.com>
2019-03-16 22:53:21 -0400

    module 5 pivoting and plotting data

--- stats/attendance.py
+++ stats/attendance.py
@@ -2,7 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from data import games
-
 attendance = games.loc[(games['type'] == 'info') & (games['multi2'] == 'attendance') , ['year','multi3']]
 attendance.columns = ['year', 'attendance']
 attendance.loc[:,'attendance'] = pd.to_numeric(attendance.loc[:,'attendance'])
