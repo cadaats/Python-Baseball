f86b75666ae78a44cf8422ee95752e00c8596350
Chandrakant <cadaats@gmail.com>
2019-03-19 05:08:28 -0400

    events+pa

--- stats/defense.py
+++ stats/defense.py
@@ -13,7 +13,8 @@ try:
     events.columns = events.columns.droplevel()
     events.columns = ['year', 'game_id', 'team', 'BB', 'E', 'H', 'HBP', 'HR', 'ROE', 'SO']
     events = events.rename_axis('None',axis='columns')
-    events_plus_pa = pd.merge(pa, events, how='outer', left_on=['year', 'game_id', 'team'], right_on=['year', 'game_id', 'team'])
+
+    events_plus_pa = pd.merge(events, pa, how='outer', left_on=['year', 'game_id', 'team'], right_on=['year', 'game_id', 'team'])
 
     defense = pd.merge(events_plus_pa, info)
     defense.loc[:,'DER'] =  1 - ((defense['H'] + defense['ROE']) / (defense['PA'] - defense['BB'] - defense['SO'] 
