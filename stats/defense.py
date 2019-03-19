5a70e55e38a519ca99abe0c5133f1603f3dbb4ed
Chandrakant <cadaats@gmail.com>
2019-03-19 05:06:12 -0400

    changed plotting for every 4 years

--- stats/defense.py
+++ stats/defense.py
@@ -13,7 +13,7 @@ try:
     events.columns = events.columns.droplevel()
     events.columns = ['year', 'game_id', 'team', 'BB', 'E', 'H', 'HBP', 'HR', 'ROE', 'SO']
     events = events.rename_axis('None',axis='columns')
-    events_plus_pa = pd.merge(pa,events, how='outer', left_on=['year', 'game_id', 'team'], right_on=['year', 'game_id', 'team'])
+    events_plus_pa = pd.merge(pa, events, how='outer', left_on=['year', 'game_id', 'team'], right_on=['year', 'game_id', 'team'])
 
     defense = pd.merge(events_plus_pa, info)
     defense.loc[:,'DER'] =  1 - ((defense['H'] + defense['ROE']) / (defense['PA'] - defense['BB'] - defense['SO'] 
@@ -23,7 +23,7 @@ try:
     der = defense.loc[defense['year'] >= 1978, ['year','defense','DER']]
     der = der.pivot(index='year',columns = 'defense', values='DER')
     
-    der.plot(x_compat=True, xticks= range(1978,2018,3), rot=45)
+    der.plot(x_compat=True, xticks= range(1978,2018,4), rot=45)
     plt.show()
     
 except ImportError:
